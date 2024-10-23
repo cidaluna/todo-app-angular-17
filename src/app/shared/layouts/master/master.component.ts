474d105240886db4c882fa034af6b99d7a7967e1
Cida Luna <cidalunna@hotmail.com>
2024-10-23 14:12:58 -0300

    Add login and todo routes

--- src/app/shared/layouts/master/master.component.ts
+++ src/app/shared/layouts/master/master.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './master.component.html',
   styleUrl: './master.component.css'
 })

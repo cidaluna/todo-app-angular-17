a5e35c5228d1c07fc9ac615d6f14e8eea2737bf1
Cida Luna <cidalunna@hotmail.com>
2024-10-23 18:45:44 -0300

    Created todo interface

---
+++ src/app/shared/models/todo.ts
@@ -0,0 +1,10 @@
+import { ITodoType } from "../components/todo-card/todo-card.component";
+
+export interface ITodo{
+  id?: number,
+  title: string,
+  description: string,
+  status: ITodoType,
+  created_at?: string,
+  updated_at?: string,
+}

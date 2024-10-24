827f6bb1ce352792525e2425a5af04f99550954d
Cida Luna <cidalunna@hotmail.com>
2024-10-24 09:48:02 -0300

    Delete unused files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { TodoComponent } from './pages/todo/todo.component';
-import { DefaultComponent } from './shared/layouts/default/default.component';
-import { MasterComponent } from './shared/layouts/master/master.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: DefaultComponent,
-    children:[{ path: '', component: LoginComponent }]
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: '',
-    component: MasterComponent,
-    children:[{ path: 'todo', component: TodoComponent }]
+    component: TodoComponent,
   },
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,22 +9,5 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  loginForm!: FormGroup;
-
-  constructor(private _fb: FormBuilder){
-    this.loginForm = this._fb.group({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', Validators.required),
-    });
-  }
-
-  onSubmit(){
-    if(this.loginForm.valid){
-      console.log("Form valid");
-    }else{
-      this.loginForm.markAllAsTouched();
-      console.log("Form invalid!");
-    }
-  }
 
 }

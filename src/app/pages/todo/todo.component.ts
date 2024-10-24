a16b45f4393187a883d17bf1bf5603d9e5c18a71
Cida Luna <cidalunna@hotmail.com>
2024-10-24 14:41:06 -0300

    Created todo

--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,16 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "../../shared/layouts/header/header.component";
-import { MaterialModule } from '../../shared/material/material/material.module';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule, MaterialModule, RouterOutlet, HeaderComponent],
+  imports: [],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
-export class TodoComponent{
+export class TodoComponent {
 
 }
